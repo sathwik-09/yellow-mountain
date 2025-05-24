3bfa19d8cc0da26ced0c7b4e80be239c7ea27caf
sathwik <enapothulasathwik09@gmail.com>
2025-05-24 21:42:33 +0530

    remove auth part

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import Contact from "./pages/Contact";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -19,8 +17,6 @@ const App = () => {
         <Route path="/services" element={<Services />} />
         <Route path="/partners" element={<Partners />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
