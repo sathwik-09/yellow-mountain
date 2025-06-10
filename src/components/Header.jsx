78ab3a8e72a623e675a0de5088c97f4676de8fb7
sathwik <enapothulasathwik09@gmail.com>
2025-06-10 21:54:45 +0530

    feat(ui): updated button styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ export default function Header() {
     <header className="w-full px-6 py-4 flex items-center justify-between border-b bg-white sticky top-0 z-50">
       <Link to="/" className="text-xl text-primary pl-2">Yellow Mountain</Link>
       <nav className="hidden md:flex gap-4">
-        <Link to="/about"><Button className="cursor-pointer" variant="ghost">About</Button></Link>
-        <Link to="/services"><Button className="cursor-pointer" variant="ghost">Services</Button></Link>
-        <Link to="/partners"><Button className="cursor-pointer" variant="ghost">Partners</Button></Link>
-        <Link to="/contact"><Button className="cursor-pointer" variant="ghost">Contact</Button></Link>
+        <Link to="/about"><Button className="cursor-pointer hover:bg-amber-300" variant="ghost">About</Button></Link>
+        <Link to="/services"><Button className="cursor-pointer hover:bg-amber-300" variant="ghost">Services</Button></Link>
+        <Link to="/partners"><Button className="cursor-pointer hover:bg-amber-300" variant="ghost">Partners</Button></Link>
+        <Link to="/contact"><Button className="cursor-pointer hover:bg-amber-300" variant="ghost">Contact</Button></Link>
       </nav>
     </header>
   );
